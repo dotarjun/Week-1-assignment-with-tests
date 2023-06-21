88873444a21bc07fc78ab375b276781644003ab7
Arjun <arjunsingh8112@gmail.com>
2023-06-21 11:11:39 +0530

    Anagram assignment done

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -9,6 +9,18 @@
 
 function isAnagram(str1, str2) {
 
+  const str1Arr = str1.toLowerCase().split('').sort();
+  const str2Arr = str2.toLowerCase().split('').sort();
+
+  for (let i = 0; i < str1Arr.length; i++) {
+    console.log(str1Arr[i], str2Arr[i]);
+    if (str1Arr[i] !== str2Arr[i]) {
+      return false;
+    }
+  }
+  return true;
 }
+
+console.log(isAnagram('Debit Card', 'Bad Credit'));
 
 module.exports = isAnagram;
